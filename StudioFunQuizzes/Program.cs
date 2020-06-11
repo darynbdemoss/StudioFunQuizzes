f3a83df00628018fa5f17a4012a11bef0872fabc
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-11 09:44:40 -0400

    Add project files.

---
+++ StudioFunQuizzes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudioFunQuizzes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
